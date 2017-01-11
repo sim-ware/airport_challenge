cdc95ad7944d2ecd67d11f460482ca9094c2de1c
Samir Gossain <samir.gossain@gmail.com>
2017-01-11 16:10:51 +0000

    User Story 2

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -1,4 +1,4 @@
-  describe "user stories" do
+  describe "User Stories" do
     # As an air traffic controller
     # So that planes can land safely at my airport
     # I would like to instruct a plane to land
@@ -6,5 +6,14 @@
       airport = Airport.new
       plane = Plane.new
       expect { airport.land(plane) }.not_to raise_error
+    end
+
+    # As an air traffic controller
+    # So planes can take off safely from my airport
+    # I would like to instruct a plane to take off
+    it 'so planes take off from airports, instruct a plane to take off' do
+      airport = Airport.new
+      plane = Plane.new
+      expect { airport.take_off(plane) }.not_to raise_error
     end
   end
